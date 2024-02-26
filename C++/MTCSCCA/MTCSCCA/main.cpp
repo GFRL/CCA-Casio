bd6d4b6591a542895f09d66546da328405feb2b6
GFRL <2200013213@stu.pku.edu.cn>
2024-02-26 20:15:05 +0800

    Add a try

--- C++/MTCSCCA/MTCSCCA/main.cpp
+++ C++/MTCSCCA/MTCSCCA/main.cpp
@@ -11,13 +11,139 @@
 #include<iomanip>
 #include <thread>
 #include <chrono>
+#include<vector>
+#include<map>
 #define ll long long
 #define maxn 10000000
 #define MTCSXS 0.1
-#define maxjs 3
+#define type_num 20
+#define maxjs 2
 using namespace std;
-int shichang = 5;
+int shichang = 500;
 int COUNT;
+
+
+// µ—È£∫1 ≤π 2∞Œ 3«π 4À´«π 5¥Ã 6ø≥ 7µ≤ 8À´µµ 9÷ÿµ≤ 
+struct Action {
+    int from, to, type;
+};
+map<int, int>Q{{1, 0}, { 2,0 }, { 3,3 }, { 4,3 }, { 5,1 }, { 6,2 }, { 7,0 }, { 8,0 }, { 9,0 }};
+map<int, int>Qb{{1, 0}, { 2,0 }, { 3,1 }, { 4,2 }, { 5,0 }, { 6,0 }, { 7,0 }, { 8,0 }, { 9,0 }};
+map<int, int>Qs{{1, 0}, { 2,0 }, { 3,0 }, { 4,0 }, { 5,1 }, { 6,2 }, { 7,0 }, { 8,0 }, { 9,0 }};
+map<int, int>Qv{{1, 0}, { 2,0 }, { 3,1 }, { 4,1 }, { 5,1 }, { 6,1 }, { 7,0 }, { 8,0 }, { 9,0 }};
+struct Game {
+    int num=2;
+    int bullet[2];
+    int HP[2];
+    int sword[2];
+    int next_sword[2];
+    Action now_action[2];
+    vector<int> Get_allowed_actions(int u) {
+        vector<int>A;
+        A.push_back(1);
+        A.push_back(2);
+        if (bullet[u] > 0)A.push_back(3);
+        if (bullet[u] > 1)A.push_back(4);
+        if (next_sword[u] > 0)A.push_back(5);
+        if (next_sword[u] > 0)A.push_back(6);
+        A.push_back(7);
+        A.push_back(8);
+        A.push_back(9);
+        return A;
+    }
+    void step(int real = 0) {
+        int Atype = now_action[0].type, Btype = now_action[1].type;
+        if (Q[Atype] > 0) {
+            if (Q[Btype] > 0) {
+                if (Q[Atype] > Q[Btype]) {
+                    HP[1] -= Qv[Atype];
+
+                }
+            }
+            else {
+                if ((Btype == 7 && Atype == 3) || (Btype == 7 && Atype == 5)
+                    || (Btype == 8 && Atype == 4) || (Btype == 9 && Atype == 6)) {
+                    int a = 1;
+                }
+                else {
+                    HP[1] -= Qv[Atype];
+                }
+            }
+            bullet[0] -= Qb[Atype];
+            next_sword[0] -= Qs[Atype];
+        }
+        else {
+            
+            if (Atype == 1) {
+                bullet[0]++;
+            }
+            else if (Atype == 2) {
+                sword[0]++;
+            }
+            next_sword[0] = sword[0];
+        }
+        swap(Atype, Btype);
+        if (Q[Atype] > 0) {
+            if (Q[Btype] > 0) {
+                if (Q[Atype] > Q[Btype]) {
+                    HP[0] -= Qv[Atype];
+
+                }
+            }
+            else {
+                if ((Btype == 7 && Atype == 3) || (Btype == 7 && Atype == 5)
+                    || (Btype == 8 && Atype == 4) || (Btype == 9 && Atype == 6)) {
+                    int a = 1;
+                }
+                else {
+                    HP[0] -= Qv[Atype];
+                }
+            }
+            bullet[1] -= Qb[Atype];
+            next_sword[1] -= Qs[Atype];
+        }
+        else {
+
+            if (Atype == 1) {
+                bullet[1]++;
+            }
+            else if (Atype == 2) {
+                sword[1]++;
+            }
+            next_sword[1] = sword[1];
+        }
+        if (real) {
+            printf("A π”√¡À%d,B π”√¡À%d\n", Btype, Atype);
+            printf("A:bullet:  %d  sword:%d/%d  ,HP:%d\n", bullet[0], next_sword[0], sword[0], HP[0]);
+            printf("B:bullet:  %d  sword:%d/%d  ,HP:%d\n", bullet[1], next_sword[1], sword[1], HP[1]);
+        }
+        return;
+    }
+    void output(int js) {
+        printf("A:bullet:  %d  sword:%d/%d  ,HP:%d\n", bullet[0], next_sword[0], sword[0], HP[0]);
+        printf("B:bullet:  %d  sword:%d/%d  ,HP:%d\n", bullet[1], next_sword[1], sword[1], HP[1]);
+        auto AA = Get_allowed_actions(js);
+        int s = AA.size();
+        cout << "Allowed action\n";
+        cout << "1 ≤π 2∞Œ 3«π 4À´«π 5¥Ã 6ø≥ 7µ≤ 8À´µµ 9÷ÿµ≤\n ";
+        for (int i = 0; i < s; i++) {
+            cout << AA[i] << " ";
+        }
+        cout << endl;
+    }
+    bool end() {
+        return HP[0] <= 0 || HP[1] <= 0;
+    }
+    double tmp_value(int js) {
+        return (bullet[js] * 5 + sword[js] * 2 + next_sword[js] + HP[js] * 8);
+    }
+    double get_value(int js) {
+        return tmp_value(js) - tmp_value(1 - js);
+    }
+}Initial;
+
+
+
 struct dian {//∞◊∆Â1;∫⁄∆Â-1;
     int x, y; double value, gz1, gz2;
     dian(int x, int y, double value, double gz1, double gz2) :x(x), y(y), value(value), gz1(gz1), gz2(gz2) {}
@@ -33,7 +159,7 @@ struct cjd {
     vector<pair<int, double>>ALLOW_TYPE_HISTORY[2];
     int fa;
     int bh;
-}CJD[100010];
+}CJD[10000];
 int CJDCNT;
 
 cjd xj(Game U, int father) {
@@ -99,7 +225,6 @@ pair<int,double> EXPAND(cjd& root,int lunshu,int js) {
     return make_pair(now_type, ss);
 }
 int AI2(int bs,int js) {
-    fill(CJD + 1, CJD + 100000, cjd{});
     CJDCNT = 0;
     COUNT = 0;
     auto start = std::chrono::system_clock::now();
@@ -116,6 +241,31 @@ int AI2(int bs,int js) {
     return final_choice;
 }
 int main() {
-    
+    printf(" ‰»ÎHP1,HP2\n");
+    cin >> Initial.HP[0] >> Initial.HP[1];
+    int count=0;
+    Initial.output(1);
+    while (!Initial.end()) {
+        int s1 = AI2(++count, 0);
+        int s2 = AI2(count, 1);
+        /*while (1)
+        {
+            cin >> s2;
+            auto AA = Initial.Get_allowed_actions(1);
+            int s = AA.size();
+            bool check = false;
+            for (int i = 0; i < s; i++) {
+                if (AA[i] == s2)check = true;
+            }
+            if (!check) {
+                cout << "WRONG!\n";
+            }
+            else {
+                break;
+            }
+        }*/
+        Initial.now_action[0].type = s1, Initial.now_action[1].type = s2;
+        Initial.step(1);
+    }
     return 0;
 }
